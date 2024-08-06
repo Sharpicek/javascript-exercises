97b1612c6e173008ed823dac60f723b68313dacb
Sharpicek <davidthomas13@proton.me>
2024-08-06 21:21:18 +0200

    Finish 07-tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5/9);
+  const celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * (9/5) + 32; 
+  const fahrenheitRounded = Math.round(fahrenheit * 10) / 10; 
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
