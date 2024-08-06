376ddd13202b9df4a799160192384fef0c821114
Sharpicek <davidthomas13@proton.me>
2024-08-06 20:36:40 +0200

    Finish 05-sumAll exercise
    Finished it by myself, but the solution method is way cleaner.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0 || firstNumber - Math.floor(firstNumber) != 0 
+    || secondNumber - Math.floor(secondNumber) != 0 || typeof firstNumber != 'number' || typeof secondNumber != 'number') {
+        return "ERROR";
+    }
+    let finalSum = 0;
+    let biggerNumber, smallerNumber;
+    if (firstNumber > secondNumber) {
+        biggerNumber = firstNumber;
+        smallerNumber = secondNumber;
+    } else {
+        biggerNumber = secondNumber;
+        smallerNumber = firstNumber;
+    }
 
+    for (let i = smallerNumber; i <= biggerNumber; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
