20edd0bc5c69bf5ab9055a6bc7c5060407868cc3
Sharpicek <davidthomas13@proton.me>
2024-08-06 17:43:52 +0200

    Finish 02-repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    let fullString = "";
+    if (numberOfRepeats < 0) {
+        return fullString = "ERROR";
+    }
+    for (i = 0; i < numberOfRepeats; i++) {
+        fullString = fullString + string;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
