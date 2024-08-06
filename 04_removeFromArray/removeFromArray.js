cdf69b300059936f98e0bd2e5bad2a66bed5f559
Sharpicek <davidthomas13@proton.me>
2024-08-06 20:06:55 +0200

    Finish 04-removeFromArray exercise
    I would not finish it without looking at the solution. I was googling for like two hours and forEach
    method came about multiple times, but I was like "I should use something that I shouldn't know yet." Its kinda frustrating.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayIn, ...exclude) {
+    const arrayOut = [];
+    arrayIn.forEach(element => {
+        if (!exclude.includes(element)) {
+            arrayOut.push(element);
+        }});
+    console.log(arrayOut);
+    return arrayOut;
 };
 
 // Do not edit below this line
