7440d227be261606c66e102e1b7b7c89b8569225
Sharpicek <davidthomas13@proton.me>
2024-08-06 18:10:39 +0200

    Finish 03-reverseString exercise
    I did it the hard way, but I now know about the split, reverse, join method.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let stringLengthCounter = string.length;
+    let fullString = "";
+    for (let i = 0; i < stringLength; i++, stringLengthCounter--) {
+        fullString += string.at(stringLengthCounter - 1);
+    } 
+    return fullString;
 };
 
 // Do not edit below this line
